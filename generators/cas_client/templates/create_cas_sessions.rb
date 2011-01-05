5c966ab8c648b65e1fea548dbe7091796bdbdd55
Goin Tan <gointan@gmail.com>
2011-01-05 13:11:52 -0500

    change datatype for to able index and improve performance

--- generators/cas_client/templates/create_cas_sessions.rb
+++ generators/cas_client/templates/create_cas_sessions.rb
@@ -1,13 +1,16 @@
 class CreateCasSessions < ActiveRecord::Migration
   def self.up
     create_table :cas_sessions do |t|
-      t.text     :service_ticket 
-      t.text  :session_id
+      t.string   :service_ticket 
+      t.string   :session_id
     end
+	
+	add_index :cas_sessions, [:service_ticket]
   end
  
   def self.down
     drop_table :cas_sessions
+	remove_index  :cas_sessions, [:service_ticket]
   end
 end
 
