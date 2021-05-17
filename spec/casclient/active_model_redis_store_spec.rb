1aea30ea6c4abb8d74197b62b2a0beaee246f97a
Vipul Patel <vipatel@mdsol.com>
2021-05-17 09:35:46 -0400

    removed extra space from true

--- spec/casclient/active_model_redis_store_spec.rb
+++ spec/casclient/active_model_redis_store_spec.rb
@@ -22,8 +22,8 @@ describe ActionDispatch::Session::ActiveModelRedisStore do
     it 'logs warning if pool dosnt contain session' do
       allow_any_instance_of(ActionDispatch::Session::RedisStore).to receive(:exist?).and_return(true)
       allow_any_instance_of(ActionDispatch::Session::ActiveModelRedisStore).to receive(:get_session).and_return( [{"id" => "abcd"},{ "session" => "12345"}] )
-      allow_any_instance_of(ActionDispatch::Session::RedisStore).to receive(:destroy_session).and_return( true)
-      allow_any_instance_of(Rack::Session::Redis).to receive(:destroy_session).and_return( true)
+      allow_any_instance_of(ActionDispatch::Session::RedisStore).to receive(:destroy_session).and_return(true)
+      allow_any_instance_of(Rack::Session::Redis).to receive(:destroy_session).and_return(true)
       logger = double('logger')
       allow(CASClient::LoggerWrapper).to receive(:new).and_return(logger)
       expect(logger).to receive(:warn).with("Session::ActiveModelRedisStore#destroy_session: the retrieved pool session for session_id 12345 is nil")
@@ -35,7 +35,7 @@ describe ActionDispatch::Session::ActiveModelRedisStore do
       allow(CASClient::LoggerWrapper).to receive(:new).and_return(logger)
       allow_any_instance_of(ActionDispatch::Session::ActiveModelRedisStore).to receive(:exist?).and_return(true, false)
       allow_any_instance_of(ActionDispatch::Session::ActiveModelRedisStore).to receive(:get_session).and_return( [{"session_id" => " test"},{ "service_ticket" => "456790"}] ,[])
-      allow_any_instance_of(Rack::Session::Redis).to receive(:destroy_session).and_return( true)
+      allow_any_instance_of(Rack::Session::Redis).to receive(:destroy_session).and_return(true)
       expect(logger).to receive(:warn).with("Session::ActiveModelRedisStore#destroy_session: [SESSION 12345] Service ticket key present, @pool.exist?: false")
       expect { subject.destroy_session '','12345', {} }.not_to raise_error
     end
